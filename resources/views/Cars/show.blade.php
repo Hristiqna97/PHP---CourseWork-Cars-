c58d4b32d5531e27dcb8cb282d1b98430d5566e5
Hristiqna97 <hristiqna1997@abv.bg>
2018-02-06 22:05:59 +0200

    Add show.view for cars,models and makes

--- resources/views/Cars/show.blade.php
+++ resources/views/Cars/show.blade.php
@@ -2,34 +2,44 @@
 
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2> Show Article</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('cars.index') }}"> Back</a>
-            </div>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+    <div class="container">
+        <h2>Информацията за колата</h2>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('makes.index') }}">Върни се към списъка</a>
         </div>
+        <table class="table">
+
+            <th style="background-color:aliceblue">Модел</th>
+            <tr>
+            <td> {{ $car->model->name}}</td>
+            </tr>
+            <th  style="background-color:aliceblue">Производител</th>
+            <tr>
+            <td> {{ $car->model->makes->name}}</td>
+            </tr>
+            <th  style="background-color:aliceblue">Година</th>
+            <tr>
+                <td>  {{$car->year}}</td>
+            </tr>
+            <th  style="background-color:aliceblue">Километри</th>
+            <tr>
+                <td>{{$car->km}}</td>
+            </tr>
+        </table>
     </div>
 
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Title:</strong>
-                {{$car}}
-                {{ $car->model->name}}
-                {{ $car->model->makes->name}}
-                {{$car->year}}
-                {{$car->km}}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Body:</strong>
-
-            </div>
-        </div>
-    </div>
+    </body>
+    </html>
 @endsection
--- resources/views/Makes/show.blade.php
+++ resources/views/Makes/show.blade.php
@@ -1,7 +1,51 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 453
- * Date: 7.1.2018 г.
- * Time: 17:26
- */
+@extends('layouts.app')
+
+
+@section('content')
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('makes.index') }}">Върни се към списъка</a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <div class="container">
+                    <h2>Информацията за производителите</h2>
+                    <table class="table">
+
+                        <th style="background-color:aliceblue">Име на производитела</th>
+                        <tr>
+                            <td> {{ $make->name}}</td>
+                        </tr>
+                        <th  style="background-color:aliceblue">Година</th>
+                        <tr>
+                            <td> {{ $make->year}}</td>
+                        </tr>
+
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+@endsection
